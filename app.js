be0cc8b43584a190c5d282847122688c63b0624c
Shadow53 <shadow53@shadow53.com>
2016-03-14 14:43:06 -0700

    Turned Student Queue into another npm plugin. This way the sensitive data isn't part of the code anymore and it can be depended on in other projects.

--- app.js
+++ app.js
@@ -10,119 +10,127 @@ var fs = require('fs');
 var path = require('path');
 var DB = require('student-queue-mysql-plugin');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use("/js", express.static(path.join(__dirname, path.join('public', 'js'))));
-app.use("/css", express.static(path.join(__dirname, path.join('public', 'css'))));
+function StudentQueue(config){
 
-var db = new DB({
-    host: "localhost",
-    user: "mnbryant_queue",
-    password: "CYL88ix4stDdxpuarm86RLjf",
-    database: "mnbryant_studentqueue",
-    table: "config"
-});
+    if (!(config.hasOwnProperty("host") && config.hasOwnProperty("user") && config.hasOwnProperty("password") &&
+        config.hasOwnProperty("database"))){
+        throw new Error("Missing one or more of the required options: host, user, password, database");
+    }
 
-var createConfig = db.createConfigTable();
+    this.db = new DB(this.config);
+}
 
-createConfig.then(
-    function(){
-        var load = db.load();
+StudentQueue.prototype.start = function(){
+    var that = this;
 
-        load.then(
-            function(){
-                app.use("/admin", express.static(path.join(__dirname, path.join('public', 'siteAdmin'))));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use("/js", express.static(path.join(__dirname, path.join('public', 'js'))));
+    app.use("/css", express.static(path.join(__dirname, path.join('public', 'css'))));
 
-                Object.keys(db.queues).forEach(function(name){
-                    app.use("/" + name, express.static(path.join(__dirname, path.join('public', 'queue'))));
+    var createConfig =that.db.createConfigTable();
 
-                    var queue = db.queues[name];
-                    io.on('connection', function(socket){
-                        console.log("Connection");
+    createConfig.then(
+        function(){
+            var load =that.db.load();
 
-                        // Only allow teacher/aide actions if authenticated
-                        socket.on('login', function(password){
-                            // Validate password against hash currently stored in text file
-                            var auth = db.validatePassword(name, password);
-                            // TODO: Change so that one login can provide multiple pages based on argument?
-                            auth.then(
-                                function(){
-                                    // Moved teacher socket handlers here so that they only work after validation
-                                    socket.on('removeRequest', function(id){
-                                        var realId = id.slice(1,7);
-                                        var type = id.slice(7);
-                                        if (type === "Help"){
-                                            queue.remove(realId).then(
-                                                function(){
-                                                    console.log("Successfully removed request from " + realId);
-                                                },
-                                                function(err){console.log(err)}
-                                            );
-                                        }
-                                        else {
-                                            console.log("Could not remove request from database");
-                                        }
-                                        console.log("Student with id " + id + " was resolved");
-                                        io.emit('removeStudent', id);
-                                    });
+            load.then(
+                function(){
+                    app.use("/admin", express.static(path.join(__dirname, path.join('public', 'siteAdmin'))));
 
-                                    socket.on('clearedAll', function(){
-                                        queue.reset();
-                                        io.emit('clearAll');
-                                    });
+                    Object.keys(db.queues).forEach(function(name){
+                        app.use("/" + name, express.static(path.join(__dirname, path.join('public', 'queue'))));
 
-                                    socket.emit('loginAuth', true);
+                        var queue =that.db.queues[name];
+                        io.on('connection', function(socket){
+                            console.log("Connection");
 
-                                    var getRequests = queue.getAll();
-                                    getRequests.then(
-                                        function(requests){
-                                            requests.forEach(function(req, i, arr){
-                                                socket.emit("addToQueue", {id: req.studentid, name:req.name, problem: req.description}) ;
-                                            });
-                                        },
-                                        function(err){
-                                            console.log(err);
-                                        }
-                                    );
-                                },
-                                function(errMsg){
-                                    socket.emit('loginAuth', false);
-                                });
-                        });
+                            // Only allow teacher/aide actions if authenticated
+                            socket.on('login', function(password){
+                                // Validate password against hash currently stored in text file
+                                var auth =that.db.validatePassword(name, password);
+                                // TODO: Change so that one login can provide multiple pages based on argument?
+                                auth.then(
+                                    function(){
+                                        // Moved teacher socket handlers here so that they only work after validation
+                                        socket.on('removeRequest', function(id){
+                                            var realId = id.slice(1,7);
+                                            var type = id.slice(7);
+                                            if (type === "Help"){
+                                                queue.remove(realId).then(
+                                                    function(){
+                                                        console.log("Successfully removed request from " + realId);
+                                                    },
+                                                    function(err){console.log(err)}
+                                                );
+                                            }
+                                            else {
+                                                console.log("Could not remove request from database");
+                                            }
+                                            console.log("Student with id " + id + " was resolved");
+                                            io.emit('removeStudent', id);
+                                        });
 
-                        socket.on('changePass', function(passObj){
-                            var auth = db.validatePassword(name, passObj.old);
-                            auth.then(function(){
-                                    if (passObj.new.length > 7){
-                                        db.setHash(name, passObj.new).then(function(){
-                                                socket.emit("changePassResult", "Updated password.");
+                                        socket.on('clearedAll', function(){
+                                            queue.reset();
+                                            io.emit('clearAll');
+                                        });
+
+                                        socket.emit('loginAuth', true);
+
+                                        var getRequests = queue.getAll();
+                                        getRequests.then(
+                                            function(requests){
+                                                requests.forEach(function(req, i, arr){
+                                                    socket.emit("addToQueue", {id: req.studentid, name:req.name, problem: req.description}) ;
+                                                });
                                             },
                                             function(err){
-                                                socket.emit("changePassResult", err.toString());
-                                            });
+                                                console.log(err);
+                                            }
+                                        );
+                                    },
+                                    function(errMsg){
+                                        socket.emit('loginAuth', false);
+                                    });
+                            });
 
-                                    }
-                                    else socket.emit("changePassResult", "Update failed. Invalid password.");
-                                },
-                                function(){
-                                    socket.emit("changePassResult", "Update failed.");
-                                });
-                        });
+                            socket.on('changePass', function(passObj){
+                                var auth =that.db.validatePassword(name, passObj.old);
+                                auth.then(function(){
+                                        if (passObj.new.length > 7){
+                                           that.db.setHash(name, passObj.new).then(function(){
+                                                    socket.emit("changePassResult", "Updated password.");
+                                                },
+                                                function(err){
+                                                    socket.emit("changePassResult", err.toString());
+                                                });
 
-                        socket.on('studentRequest', function(student){
-                            console.log("Received help request");
+                                        }
+                                        else socket.emit("changePassResult", "Update failed. Invalid password.");
+                                    },
+                                    function(){
+                                        socket.emit("changePassResult", "Update failed.");
+                                    });
+                            });
 
-                            queue.add(student);
+                            socket.on('studentRequest', function(student){
+                                console.log("Received help request");
 
-                            // Send to teacher page
-                            io.emit('addToQueue', student);
+                                queue.add(student);
+
+                                // Send to teacher page
+                                io.emit('addToQueue', student);
+                            });
                         });
                     });
-                });
-            }
-        );
-    }
-)
+                }
+            );
+        }
+    );
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+    http.listen(3000, function(){
+        console.log('listening on *:3000');
+    });
+};
+
+module.exports = StudentQueue;
