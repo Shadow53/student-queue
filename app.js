75ad60a7ec408c287ab026625dab889cddd11d45
Shadow53 <shadow53@shadow53.com>
2016-03-21 20:22:25 -0700

    One final commit for now: added queues are now usable without restarting the server.

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ StudentQueue.prototype.start = function(){
                                         if (queue.hasOwnProperty("name") && queue.hasOwnProperty("password")){
                                             that.db.addNewQueue(queue).then(
                                                 function(){
+                                                    initQueue(queue.name);
                                                     socket.emit("addedNewQueue");
                                                 },
                                                 function(err){
@@ -114,97 +115,105 @@ StudentQueue.prototype.start = function(){
                         });
                     });
 
-                    Object.keys(that.db.queues).forEach(function(name){
-                        app.use("/" + name.toLowerCase(), express.static(path.join(__dirname, path.join('public', 'queue'))));
-
-                        var queue = that.db.queues[name];
-                        var room = io.of("/" + name.toLowerCase());
-                        room.on('connection', function(socket){
-                            console.log("Connection");
-
-                            // Only allow teacher/aide actions if authenticated
-                            socket.on('login', function(password){
-                                // Validate password against hash currently stored in text file
-                                var auth = that.db.validatePassword(name, password);
-                                // TODO: Change so that one login can provide multiple pages based on argument?
-                                auth.then(
-                                    function(){
-                                        // Moved teacher socket handlers here so that they only work after validation
-                                        socket.on('removeRequest', function(id){
-                                            var realId = id.slice(1,7);
-                                            var type = id.slice(7);
-                                            if (type === "Help"){
-                                                queue.remove(realId).then(
-                                                    function(){
-                                                        console.log("Successfully removed request from " + realId);
-                                                    },
-                                                    function(err){console.log(err)}
-                                                );
-                                            }
-                                            else {
-                                                console.log("Could not remove request from database");
-                                            }
-                                            console.log("Student with id " + id + " was resolved");
-                                            room.emit('removeStudent', id);
-                                        });
-
-                                        socket.on('clearedAll', function(){
-                                            queue.reset();
-                                            room.emit('clearAll');
-                                        });
-
-                                        socket.emit('loginAuth', true);
-
-                                        var getRequests = queue.getAll();
-                                        getRequests.then(
-                                            function(requests){
-                                                requests.forEach(function(req, i, arr){
-                                                    socket.emit("addToQueue", {id: req.studentid, name:req.name, problem: req.description}) ;
-                                                });
-                                            },
-                                            function(err){
-                                                console.log(err);
-                                            }
-                                        );
-                                    },
-                                    function(errMsg){
-                                        socket.emit('loginAuth', false);
-                                    });
-                            });
-
-                            socket.on('changePass', function(passObj){
-                                var auth = that.db.validatePassword(name, passObj.old);
-                                auth.then(function(){
-                                        if (passObj.new.length > 7){
-                                           that.db.setHash(name, passObj.new).then(function(){
-                                                    socket.emit("changePassResult", "Updated password.");
-                                                },
-                                                function(err){
-                                                    socket.emit("changePassResult", err.toString());
-                                                });
-
-                                        }
-                                        else socket.emit("changePassResult", "Update failed. Invalid password. Must be at least 8 characters long");
-                                    },
-                                    function(){
-                                        socket.emit("changePassResult", "Update failed.");
-                                    });
-                            });
-
-                            socket.on('studentRequest', function(student){
-                                console.log("Received help request");
-
-                                queue.add(student);
-
-                                // Send to teacher page
-                                room.emit('addToQueue', student);
-                            });
-                        });
-                    });
+                    Object.keys(that.db.queues).forEach(initQueue);
                 }
             );
         }
     );
+
+    function initQueue(name) {
+        app.use("/" + name.toLowerCase(), express.static(path.join(__dirname, path.join('public', 'queue'))));
+
+        var queue = that.db.queues[name];
+        var room = io.of("/" + name.toLowerCase());
+        room.on('connection', function (socket) {
+            console.log("Connection");
+
+            // Only allow teacher/aide actions if authenticated
+            socket.on('login', function (password) {
+                // Validate password against hash currently stored in text file
+                var auth = that.db.validatePassword(name, password);
+                // TODO: Change so that one login can provide multiple pages based on argument?
+                auth.then(
+                    function () {
+                        // Moved teacher socket handlers here so that they only work after validation
+                        socket.on('removeRequest', function (id) {
+                            var realId = id.slice(1, 7);
+                            var type = id.slice(7);
+                            if (type === "Help") {
+                                queue.remove(realId).then(
+                                    function () {
+                                        console.log("Successfully removed request from " + realId);
+                                    },
+                                    function (err) {
+                                        console.log(err)
+                                    }
+                                );
+                            }
+                            else {
+                                console.log("Could not remove request from database");
+                            }
+                            console.log("Student with id " + id + " was resolved");
+                            room.emit('removeStudent', id);
+                        });
+
+                        socket.on('clearedAll', function () {
+                            queue.reset();
+                            room.emit('clearAll');
+                        });
+
+                        socket.emit('loginAuth', true);
+
+                        var getRequests = queue.getAll();
+                        getRequests.then(
+                            function (requests) {
+                                requests.forEach(function (req, i, arr) {
+                                    socket.emit("addToQueue", {
+                                        id: req.studentid,
+                                        name: req.name,
+                                        problem: req.description
+                                    });
+                                });
+                            },
+                            function (err) {
+                                console.log(err);
+                            }
+                        );
+                    },
+                    function (errMsg) {
+                        socket.emit('loginAuth', false);
+                    });
+            });
+
+            socket.on('changePass', function (passObj) {
+                var auth = that.db.validatePassword(name, passObj.old);
+                auth.then(function () {
+                        if (passObj.new.length > 7) {
+                            that.db.setHash(name, passObj.new).then(function () {
+                                    socket.emit("changePassResult", "Updated password.");
+                                },
+                                function (err) {
+                                    socket.emit("changePassResult", err.toString());
+                                });
+
+                        }
+                        else socket.emit("changePassResult", "Update failed. Invalid password. Must be at least 8 characters long");
+                    },
+                    function () {
+                        socket.emit("changePassResult", "Update failed.");
+                    });
+            });
+
+            socket.on('studentRequest', function (student) {
+                console.log("Received help request");
+
+                queue.add(student);
+
+                // Send to teacher page
+                room.emit('addToQueue', student);
+            });
+        });
+    }
 
     http.listen(3000, function(){
         console.log('listening on *:3000');
